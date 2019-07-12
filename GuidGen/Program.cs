b5a4f9c300117ac3a9084abdd7ceaf9d56e714d8
Simon Cen <scen@affinityesolutions.com>
2019-07-12 15:15:05 -0400

    First commit

---
+++ GuidGen/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace GuidGen
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.White;
+			PrintInfo();
+
+			Console.ForegroundColor = ConsoleColor.DarkCyan;
+			PrintPrompt();
+
+			Console.ForegroundColor = ConsoleColor.Cyan;
+
+			var input = Console.ReadLine().ToLower();
+
+			while (input != "exit")
+			{
+				if (input == "help")
+				{
+					Console.ForegroundColor = ConsoleColor.White;
+					PrintHelp();
+					Console.ForegroundColor = ConsoleColor.Cyan;
+				}
+				else if (input == "clear")
+				{
+					Console.Clear();
+					Console.ForegroundColor = ConsoleColor.White;
+					PrintInfo();
+				}
+				else if (Int32.TryParse(input, out var numOfGuidToGenerate))
+				{
+					for (int i = 0; i < numOfGuidToGenerate; i++)
+					{
+						Console.WriteLine();
+						Console.ForegroundColor = ConsoleColor.Yellow;
+						Console.WriteLine($"{i + 1})");
+						Console.ForegroundColor = ConsoleColor.White;
+						Console.WriteLine(Guid.NewGuid().ToString().ToUpper());
+						Console.ForegroundColor = ConsoleColor.Cyan;
+						Console.WriteLine();
+					}
+				}
+				else if (input == "")
+				{
+					// do nothing
+				}
+				else
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+
+					Console.WriteLine($"Error: invalid command {input}");
+
+					Console.ForegroundColor = ConsoleColor.Cyan;
+				}
+
+				Console.ForegroundColor = ConsoleColor.DarkCyan;
+				PrintPrompt();
+				Console.ForegroundColor = ConsoleColor.Cyan;
+
+				input = Console.ReadLine();
+			}
+		}
+
+		private static void PrintInfo()
+		{
+			Console.WriteLine("**************************************");
+			Console.WriteLine("*                                    *");
+			Console.WriteLine("* This is a beautiful Guid generator *");
+			Console.WriteLine("*                                    *");
+			Console.WriteLine("* Enter \"help\" to see help info      *");
+			Console.WriteLine("*                                    *");
+			Console.WriteLine("**************************************");
+			Console.WriteLine();
+		}
+
+		private static void PrintPrompt()
+		{
+			Console.Write("GuidGen$: ");
+		}
+
+		private static void PrintHelp()
+		{
+			Console.WriteLine();
+			Console.WriteLine(" -- enter any number to generate {x} number of Guid you want to generate");
+			Console.WriteLine();
+			Console.WriteLine(" -- enter \"clear\" to clean up this beautiful app");
+			Console.WriteLine();
+			Console.WriteLine(" -- enter \"exit\" to exit the this beautiful app");
+			Console.WriteLine();
+			Console.WriteLine();
+		}
+	}
+}
